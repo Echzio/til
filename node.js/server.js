10010465965a81be46a86f7ce24a0ca74ef3836f
Echzio <aleksassincreed@yandex.ru>
2018-12-13 17:06:04 +0300

    [ADD] watch header

--- node.js/server.js
+++ node.js/server.js
@@ -6,14 +6,21 @@ const server = new http.Server(function (req, res) {
     // смотрим что получаем
     // console.log( req.method, req.url);
 
+    // просмотр заголовков
+    console.log(req.headers);
+
     // детальный просмотр
+    
     const urlParsed = url.parse(req.url, true);
     if (urlParsed.pathname == '/awesome' && urlParsed.query.message) {
+        res.setHeader('Cache-control', 'no-cache'); // передаем заголовок если хотим
+        res.statusCode = 200; // OK
         res.end(urlParsed.query.message);
     } else {
-        res.statusCode = 404;
+        res.statusCode = 404; // Not Found
         res.end('Page not found');
     }
+    
 });
 
 

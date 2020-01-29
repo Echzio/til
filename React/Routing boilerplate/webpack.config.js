e4e886f311dbcb83e9afe334bd13a1dde72f7b3e
Alexander <Aleksassincreed@yandex.ru>
2020-01-29 12:10:33 +0300

    Update webpack.config.js

--- React/Routing boilerplate/webpack.config.js
+++ React/Routing boilerplate/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
           options: {
             plugins: [
               postcssPresetEnv({
-                state: 0,
+                stage: 0,
                 autoprefixer: {
                   grid: true,
                 },

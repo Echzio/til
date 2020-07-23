50919f875396612f428d6d5cf754d5f45bd15d18
Echzio <aleksassincreed@yandex.ru>
2020-07-23 23:34:11 +0300

    [wip]

--- js/beLodash/redux.js
+++ js/beLodash/redux.js
@@ -1,22 +1,36 @@
 function createStore(reducer, initialState) {
   let store = initialState || reducer(undefined, { type: 'get_state' });
-  let listener = () => { };
+  let listener = [];
 
   return {
     dispatch: action => {
       store = reducer(store, action);
-      listener();
+      listener.forEach(observer => {
+        observer()
+      })
     },
     getState: () => store,
     subscribe: fn => {
-      listener = fn
+      listener = [...listener, fn]
       return () => {
-        listener = () => { }
+        listener = listener.filter((_, index, array) => {
+          return index !== (array.length - 1)
+        })
       }
     }
   }
 }
 
+function combineReducers(reducers) {
+  return (store, action) => {
+    return Object.entries(reducers).reduce((acc, [key, reducer]) => {
+      return {
+        ...acc,
+        [key]: reducer(store === undefined ? store : store[key], action)
+      }
+    }, {})
+  }
+}
 
 function counter(state = 0, action) {
   switch (action.type) {
@@ -28,10 +42,34 @@ function counter(state = 0, action) {
       return state
   }
 }
-const store = createStore(counter)
+
+function writter(state = 'hello', action) {
+  switch (action.type) {
+    case 'WORLD':
+      return state + action.payload
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  counter,
+  writter,
+})
+
+const store = createStore(rootReducer)
+
 store.subscribe(() =>
   console.log(store.getState())
 )
+
+const unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+unsubscribe()
+
 store.dispatch({ type: 'INCREMENT' })
 store.dispatch({ type: 'INCREMENT' })
 store.dispatch({ type: 'DECREMENT' })
+store.dispatch({ type: 'WORLD', payload: 'payload' })

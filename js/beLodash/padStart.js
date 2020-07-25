3915529df27ba26b57b98678d9c48d42947cdf76
Echzio <aleksassincreed@yandex.ru>
2020-07-25 21:54:03 +0300

    [wip]

---
+++ js/beLodash/padStart.js
@@ -0,0 +1,10 @@
+function padStart(words, num, payload) {
+    const count = num - words.length;
+    if (count < 1) return words
+    const currentPad = Array(count).fill(payload).join('').slice(0, count)
+    return currentPad + words
+}
+
+console.log(
+    padStart('world', 10, 'hello') === 'world'.padStart(10, 'hello')
+)

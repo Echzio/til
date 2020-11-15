0288291f1ae6c89f88d64e1c5afdb8d428d94029
Echzio <aleksassincreed@yandex.ru>
2020-11-15 14:20:27 +0300

    [wip]

--- js/beLodash/splitted.js
+++ js/beLodash/splitted.js
@@ -1,20 +1,28 @@
 function splitted(arr, count) {
-    return Object.values(arr.reduce((acc, el, index) => {
-        let counter = (acc.counter || 0);
-        if (index % count === 0) counter++;
-        acc = {
-            ...acc,
-            counter,
-            [counter]: [...(acc[counter] || []), el]
-        }
-        return acc
-    }, {})).slice(0, -1)
+  return Object.values(
+    arr.reduce((acc, el, index) => {
+      let counter = acc.counter || 0;
+      if (index % count === 0) counter++;
+      acc = {
+        ...acc,
+        counter,
+        [counter]: [...(acc[counter] || []), el],
+      };
+      return acc;
+    }, {})
+  ).slice(0, -1);
 }
 
 const arr = Array.from({ length: 51 }, (_, index) => ({
-    name: index,
-}))
+  name: index,
+}));
 
-console.log(
-    splitted(arr, 4)
-)
+function splitted(arr, count) {
+  let res = [];
+  for (let i = 0; i < arr.length; i += count) {
+    res = [...res, arr.slice(i, i + count)];
+  }
+  return res;
+}
+
+console.log(splitted(arr, 4));

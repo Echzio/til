15342d2068267fd9047c55f60bce2f81c9bfc6b6
Echzio <aleksassincreed@yandex.ru>
2021-01-15 23:11:14 +0300

    [wip]

--- js/beLodash/recursion.js
+++ js/beLodash/recursion.js
@@ -1,95 +1,99 @@
 const company = {
-    sales: [{
-        name: 'John',
-        salary: 1000
-    }, {
-        name: 'Alice',
-        salary: 600
-    }],
-    development: {
-        sites: [{
-            name: 'Peter',
-            salary: 2000
-        }, {
-            name: 'Alex',
-            salary: 1800
-        }],
-        internals: [{
-            name: 'Jack',
-            salary: 1300
-        }]
-    }
-}
+  sales: [
+    {
+      name: "John",
+      salary: 1000,
+    },
+    {
+      name: "Alice",
+      salary: 600,
+    },
+  ],
+  development: {
+    sites: [
+      {
+        name: "Peter",
+        salary: 2000,
+      },
+      {
+        name: "Alex",
+        salary: 1800,
+      },
+    ],
+    internals: [
+      {
+        name: "Jack",
+        salary: 1300,
+      },
+    ],
+  },
+};
 
 const list = {
-    value: 1,
+  value: 1,
+  next: {
+    value: 2,
     next: {
-        value: 2,
+      value: 3,
+      next: {
+        value: 4,
         next: {
+          value: 2,
+          next: {
             value: 3,
             next: {
-                value: 4,
+              value: 4,
+              next: {
+                value: 2,
                 next: {
-                    value: 2,
+                  value: 3,
+                  next: {
+                    value: 4,
                     next: {
+                      value: 2,
+                      next: {
                         value: 3,
                         next: {
-                            value: 4,
-                            next: {
-                                value: 2,
-                                next: {
-                                    value: 3,
-                                    next: {
-                                        value: 4,
-                                        next: {
-                                            value: 2,
-                                            next: {
-                                                value: 3,
-                                                next: {
-                                                    value: 4,
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+                          value: 4,
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 };
 
 function calculateSalary(data) {
-    if (Array.isArray(data)) {
-        return data.reduce((acc, { salary }) => acc + salary, 0)
-    } else {
-        let total = 0;
-        Object.values(data).forEach(item => {
-            total += calculateSalary(item)
-        })
-        return total
-    }
+  if (Array.isArray(data)) {
+    return data.reduce((acc, { salary }) => acc + salary, 0);
+  } else {
+    return Object.values(data).reduce((acc, el) => acc + calculateSalary(el), 0)
+  }
 }
 
 function calculateSum(value) {
-    return value > 1 ? value + calculateSum(value - 1) : value
+  return value > 1 ? value + calculateSum(value - 1) : value;
 }
 
 function factorial(value) {
-    return value > 1 ? value * factorial(value - 1) : value
+  return value > 1 ? value * factorial(value - 1) : value;
 }
 
 function fibonachi(value) {
-    return value >= 1 ? fibonachi(value - 1) + fibonachi(value - 2) : 1
+  return value >= 1 ? fibonachi(value - 1) + fibonachi(value - 2) : 1;
 }
 
 function writeList(data) {
-    return Object.values(data).reduce((acc, el) => {
-        const res = Object(el) === el ? writeList(el) : el;
-        return acc + res
-    }, 0)
+  return Object.values(data).reduce((acc, el) => {
+    const res = Object(el) === el ? writeList(el) : el;
+    return acc + res;
+  }, 0);
 }
 
-console.log(writeList(list))
+console.log(calculateSalary(company));

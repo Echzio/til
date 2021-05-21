823ea3162beda6386f0f35c7527ec7ae47d312ec
Echzio <aleksassincreed@yandex.ru>
2021-05-22 02:14:26 +0300

    [fix]

--- js/beLodash/bubbleSort.mjs
+++ js/beLodash/bubbleSort.mjs
@@ -1,7 +1,7 @@
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0; i <= arr.length - 1; i++) {
     let wasSwap = false;
-    for (let j = 0; j < arr.length - 1 - i; j++) {
+    for (let j = 0; j <= arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         wasSwap = true;

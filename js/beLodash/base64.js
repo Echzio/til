dd80380b1d70a52e93d88b94dfe58fc08221da42
Alexander Komarov <AKomarov2@alfabank.ru>
2022-08-22 19:52:00 +0300

    feat(*): add base64 manipulations

---
+++ js/beLodash/base64.js
@@ -0,0 +1,50 @@
+const fromBase64 = () => {
+  const parsed = [26, 6, 21, 44, 27, 6, 60, 32, 29, 54, 61, 50, 27, 6, 16]; // with base64 encoding table
+
+  const binary6 = parsed
+    .map((item) => item.toString(2).padStart(6, "0"))
+    .join("");
+
+  const binary8 = [];
+
+  for (let i = 0; i < binary6.length; i += 8) {
+    binary8.push(binary6.slice(i, i + 8).padStart(8, "0"));
+  }
+
+  return binary8.map((item) => String.fromCodePoint(parseInt(item, 2)));
+};
+
+const toBase64 = () => {
+  const string = "hello world";
+
+  const binary = [...string]
+    .map((item) => item.charCodeAt(0).toString(2).padStart(8, "0"))
+    .join("");
+
+  const binary6 = []; // with base64 encoding table
+
+  for (let i = 0; i < binary.length; i += 6) {
+    binary6.push(parseInt(binary.slice(i, i + 6).padStart(6, "0"), 2));
+  }
+
+  return binary6;
+};
+
+const base64ToBlob = () => {
+  const raw = atob("base64 string");
+
+  const arrayWithUnit8 = [];
+
+  for (let i = 0; i < raw.length; i += 512) {
+    const sliced = raw.slice(i, i + 512);
+
+    arrayWithUnit8.push(
+      new Uint8Array([...sliced].map((item) => item.charCodeAt(0)))
+    );
+  }
+
+  const blob = new Blob(arrayWithUnit8, { type: "image/jpeg" });
+
+  return URL.createObjectURL(blob);
+};
+
